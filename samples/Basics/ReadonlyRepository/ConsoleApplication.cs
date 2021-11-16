cc5b09ed70d4f1594a67de2c24b5d3071517f97d
Verthe Bart <Bart.Verthe@UZGENT.be>
2021-11-16 01:37:51 +0100

    modernize ReadonlyRepository sample

---
+++ samples/Basics/ReadonlyRepository/ConsoleApplication.cs
@@ -0,0 +1,60 @@
+﻿using Aeon.Core.Repository;
+using Aeon.Core.Repository.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Models;
+
+internal sealed class ConsoleApplication : IHostedService {
+
+    private readonly IReadonlyRepository<PostCountByBlogs> _postCountByBlogsRepository;
+
+    /// <summary>c'tor</summary> 
+    /// <param name="postCountByBlogsRepository"></param>
+    /// <param name="serviceProvider">Only injected so we can access DbContext to create our database in SetupDatabase</param>
+    public ConsoleApplication(IReadonlyRepository<PostCountByBlogs> postCountByBlogsRepository,
+                              IServiceProvider serviceProvider) {
+        _postCountByBlogsRepository = postCountByBlogsRepository;
+        SetupDatabase(serviceProvider);
+    }
+
+    /// <summary>(re)-create the sample database</summary>
+    private static void SetupDatabase(IServiceProvider serviceProvider) {
+        using (var scope = serviceProvider.CreateScope()) {
+            var dbContext = scope.ServiceProvider.GetService<BloggingContext>();
+            // (re-)create the database
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            // create the database view "PostCountByBlogs"              
+            dbContext.Database.ExecuteSqlRaw(
+                @"CREATE VIEW PostCountByBlogs 
+                              AS 
+                                SELECT Blog.BlogId, Blog.Name, COUNT(PostId) Total FROM Blog
+                                INNER JOIN Post ON Post.BlogId = Blog.BlogId
+                                GROUP BY Blog.BlogId 
+                                ORDER BY COUNT(PostId) DESC"
+            );
+        }
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken) {
+        // construct an include directive for the "PostCountByBlogs" database view, including the Blog and the Blog's Posts
+        var postCountByBlogsIncludeBlog = new RepositoryInclude<PostCountByBlogs>();
+        postCountByBlogsIncludeBlog.Include(v => v.Blog).ThenInclude(b => b.Posts);
+
+        // get all PostCountByBlogs with the constructed include
+        var viewData = await _postCountByBlogsRepository.AllAsync(postCountByBlogsIncludeBlog);
+
+        // print all 
+        foreach (var blogPostsCount in viewData) {
+            Console.WriteLine($"-= {blogPostsCount.BlogName} =- (#{ blogPostsCount.Total})");
+            foreach (var post in blogPostsCount.Blog.Posts) {
+                Console.WriteLine($" ** {post.PostId}: {post.Title}");
+            }
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+}
--- samples/Basics/ReadonlyRepository/Program.cs
+++ samples/Basics/ReadonlyRepository/Program.cs
@@ -1,74 +1,31 @@
 ﻿using Aeon.Core.Repository;
 using Aeon.Core.Repository.Infrastructure;
-using Aeon.Samples.Basics.ReadonlyRepository.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Models;
 
-namespace Aeon.Samples.Basics.ReadonlyRepository {
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices((services) => {
+        services.AddDbContext<BloggingContext>(options => options.UseSqlite($"Filename=./blog.db"));
 
-    public class Runnable {
+        // aeon: Register repositories
 
-        private readonly IReadonlyRepository<PostCountByBlogs> _postCountByBlogsRepository;
+        // aeon: Register a Blog-Repository as a IReadonlyRepository using the Blog Model and BloggingContext DbContext
+        services.AddScoped<IReadonlyRepository<Blog>, Repository<Blog, BloggingContext>>();
 
-        public Runnable(IReadonlyRepository<PostCountByBlogs> postCountByBlogsRepository) {
-            _postCountByBlogsRepository = postCountByBlogsRepository;
-        }
+        // aeon: Register a Post-ReadonlyRepository as a IReadonlyRepository using the Post Model and BloggingContext DbContext
+        services.AddScoped<IReadonlyRepository<Post>, ReadonlyRepository<Post, BloggingContext>>();
 
-        public void Run() {
-            // retrieve data from the "PostCountByBlogs" database View, including the Blog and from Blog its Posts
-            var postCountByBlogsIncludeBlog = new RepositoryInclude<PostCountByBlogs>();
-            postCountByBlogsIncludeBlog.Include(v => v.Blog).ThenInclude(b => b.Posts);
+        // aeon: Register a PostCountByBlogs-ReadonlyRepository as a IReadonlyRepository using the PostCountByBlogs Model and BloggingContext DbContext
+        // aeon: PostCountByBlogs refers to a View in the database
+        services.AddScoped<IReadonlyRepository<PostCountByBlogs>, ReadonlyRepository<PostCountByBlogs, BloggingContext>>();
 
-            var viewData = _postCountByBlogsRepository.All(postCountByBlogsIncludeBlog);
-
-            foreach (var blogPostsCount in viewData) {
-                Console.WriteLine($"{blogPostsCount.BlogUrl} (#Posts:{blogPostsCount.Total})");
-                foreach (var post in blogPostsCount.Blog.Posts) {
-                    Console.WriteLine($"\t[{post.PostId}]: {post.Title}");
-                }
-            }
-        }
-    }
-
-    class Program {
-
-        static void Main() {
-            var services = new ServiceCollection().AddSingleton<Runnable>();
-            services.AddDbContext<BloggingContext>(options => options.UseSqlite("Filename=./blog.db"));
-
-            // aeon: Register repositories
-
-            // aeon: Register a Blog-Repository as a IReadonlyRepository using the Blog Model and BloggingContext DbContext
-            services.AddScoped<IReadonlyRepository<Blog>, Repository<Blog, BloggingContext>>();
-
-            // aeon: Register a Post-ReadonlyRepository as a IReadonlyRepository using the Post Model and BloggingContext DbContext
-            services.AddScoped<IReadonlyRepository<Post>, ReadonlyRepository<Post, BloggingContext>>();
-
-            // aeon: Register a PostCountByBlogs-ReadonlyRepository as a IReadonlyRepository using the PostCountByBlogs Model and BloggingContext DbContext
-            // aeon: PostCountByBlogs refers to a View in the database
-            services.AddScoped<IReadonlyRepository<PostCountByBlogs>, ReadonlyRepository<PostCountByBlogs, BloggingContext>>();
-
-            var serviceProvider = services.BuildServiceProvider();
-
-            var dbContext = serviceProvider.GetService<BloggingContext>();
-            dbContext.Database.EnsureCreated();
-
-            // re-create the database view "PostCountByBlogs"  
-            dbContext.Database.ExecuteSqlRaw("DROP VIEW IF EXISTS PostCountByBlogs;");
-
-            dbContext.Database.ExecuteSqlRaw(
-            @"CREATE VIEW PostCountByBlogs 
-                  AS 
-                    SELECT Blog.BlogId, Blog.Url, COUNT(PostId) Total FROM Blog
-                    INNER JOIN Post ON Post.BlogId = Blog.BlogId
-                    GROUP BY Blog.Url 
-                    ORDER BY COUNT(PostId) DESC"
-            );
+        services.AddHostedService<ConsoleApplication>();
+    })
+    .ConfigureLogging(logging => logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning))
+    .Build()
+    .Run();
 
 
-            serviceProvider.GetService<Runnable>().Run();
-        }
-    }
-
-}
